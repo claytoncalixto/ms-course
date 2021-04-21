5d9bd0f05c8f831df9ce1ec87d92c1fc44be044a
claytoncalixto <claytonrcalixto@gmail.com>
2021-04-21 16:29:07 -0300

    Project hr-payroll created

---
+++ hr-payroll/src/test/java/com/claytoncalixto/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claytoncalixto.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
