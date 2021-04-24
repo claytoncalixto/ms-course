682037b74a389de10e491668ac3b3254280b1574
claytoncalixto <claytonrcalixto@gmail.com>
2021-04-24 16:32:00 -0300

    Ribbon load balancing

--- hr-payroll/src/main/java/com/claytoncalixto/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/claytoncalixto/hrpayroll/services/PaymentService.java
@@ -3,6 +3,7 @@ package com.claytoncalixto.hrpayroll.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.claytoncalixto.hrpayroll.entities.Payment;
 import com.claytoncalixto.hrpayroll.entities.Worker;
@@ -11,15 +12,10 @@ import com.claytoncalixto.hrpayroll.feignclients.WorkerFeignClient;
 @Service
 public class PaymentService {
 	
-	@Value("${hr-worker.host}")
-	private String workerHost;
-	
-	@Autowired
+   	@Autowired
 	private WorkerFeignClient workerFeignClient;
 
-	public Payment getPayment(long workerId, int days) {
-		
-		
+	public Payment getPayment(long workerId, int days) {		
 		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
